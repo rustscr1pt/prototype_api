a8a2eec0d32a35981c361addd98be55758188f46
rust_lang_lover <rust_lang_lover@proton.me>
2024-02-27 15:38:09 +0300

    Added filter for concrete request + changed DataModels

--- src/data_models.rs
+++ src/data_models.rs
@@ -1,11 +1,19 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Serialize)]
 pub struct SqlStream {
     pub id : u16,
     pub name : String,
+    pub brand : String,
+    pub description : String,
+    pub group_type : String,
+    pub price : u32,
     pub image_path : String,
-    pub price : u32
+    pub available_quantity : u32
+}
+#[derive(Debug, Serialize)]
+pub struct ToCompare {
+    pub compared : String
 }
 #[derive(Debug, Serialize)]
 pub struct Message {
--- src/warp_handlers.rs
+++ src/warp_handlers.rs
@@ -4,20 +4,22 @@ use mysql::prelude::Queryable;
 use tokio::sync::Mutex;
 use warp::{Rejection, Reply};
 use warp::reply::json;
-use crate::data_models::{Message, SqlStream};
+use crate::data_models::{Message, SqlStream, ToCompare};
 
 type WebResult<T> = Result<T, Rejection>;
 
-fn reply_error(e : Error) -> WebResult<impl Reply> {return Ok(json(&Message {reply : e.to_string()}))}
-
 pub async fn get_all_items_catalog(pool : Arc<Mutex<PooledConn>>) -> WebResult<impl Reply> {
     let mut unlocked = pool.lock().await;
-    match unlocked.query_map("SELECT * FROM `site_data`", |(id, name, image_path, price)| {
+    match unlocked.query_map("SELECT * FROM `items_data`", |(id, name, brand, description, group_type, price, image_path, available_quantity)| {
         SqlStream {
             id: id,
             name: name,
-            image_path: image_path,
+            brand: brand,
+            description: description,
+            group_type: group_type,
             price: price,
+            image_path: image_path,
+            available_quantity: available_quantity
         }
     }, ) {
         Ok(vector) => {
@@ -30,6 +32,37 @@ pub async fn get_all_items_catalog(pool : Arc<Mutex<PooledConn>>) -> WebResult<i
 }
 
 pub async fn get_concrete_items_catalog(value : String, pool : Arc<Mutex<PooledConn>>) -> WebResult<impl Reply> {
-    let unlocked = pool.lock().await;
-
+    let mut unlocked = pool.lock().await;
+    match unlocked.query_map("SELECT group_type FROM items_data", |(group_type)| {
+        ToCompare{ compared: group_type }
+    },
+    ) {
+        Ok(vec) => {
+            for elements in vec {
+                if value == elements.compared {
+                    match unlocked.query_map(format!(r#"SELECT * FROM `items_data` WHERE group_type = "{}""#, value),
+                                       |(id, name, brand, description, group_type, price, image_path, available_quantity)| {
+                                           SqlStream {
+                                               id: id,
+                                               name: name,
+                                               brand: brand,
+                                               description: description,
+                                               group_type: group_type,
+                                               price: price,
+                                               image_path: image_path,
+                                               available_quantity: available_quantity
+                                           }
+                                       }
+                    ) {
+                        Ok(result) => {return Ok(json(&result))}
+                        Err(e) => {return Ok(json(&Message{reply : e.to_string()}))}
+                    }
+                }
+            }
+            return Ok(json(&Message{reply : format!("{} - No values found for your request", value)}))
+        }
+        Err(e) => {
+            return Ok(json(&Message{reply : e.to_string()}))
+        }
+    }
 }
